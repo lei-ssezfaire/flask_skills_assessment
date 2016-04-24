08024bc7fdc73476231e6aad6e43b0f030ea72ac
Leilenah Mamea <leilenahmamea@yahoo.com>
2016-04-23 17:57:08 -0700

    Added application-form, application confirmation and index

---
+++ application.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index_page():
+    """Route directly to application form"""
+
+    return render_template("index.html")
+    
+
+@app.route("/application-form")
+def handle_form():
+	"""Handles the submission of applicant information form"""
+
+	return render_template("application-form.html")
+
+
+@app.route("/application-confirmation")
+def application_confirmation():
+	"""Displays application confirmation message"""
+
+	first_name = request.args.get("first-name")
+	last_name = request.args.get("last-name")
+	job_title = request.args.get("job-title")
+	salary = request.args.get("salary-requirement")
+
+	return render_template("application-response.html",
+							first_name=first_name,
+							last_name=last_name,
+							job_title=job_title,
+							salary=salary)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
